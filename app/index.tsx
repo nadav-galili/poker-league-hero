341ed6bd36746848d3f4c8a16cb5935e919e11a7
nadav-mobileBrain <nadavg@mobile-brain.net>
2025-08-10 16:18:37 +0300

    finished with google sign in

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
 import LoginForm from "@/components/LoginForm";
+import { BASE_URL } from "@/constants";
 import { useAuth } from "@/context/auth";
+import { useState } from "react";
 import { ActivityIndicator, Button, Text, View } from "react-native";
 
 export default function Index() {
-  const { user, isLoading, signOut } = useAuth();
+  const { user, isLoading, signOut, fetchWithAuth } = useAuth();
+  const [data, setData] = useState();
 
   if (isLoading) {
     return (
@@ -17,6 +20,18 @@ export default function Index() {
     return <LoginForm />;
   }
 
+  async function getProtectedData() {
+    const response = await fetchWithAuth(`${BASE_URL}/api/protected/data`, {
+      method: "GET",
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      setData(data);
+    }
+  }
+
   return (
     <View
       style={{
@@ -27,7 +42,9 @@ export default function Index() {
       <Text>{user.sub}</Text>
       <Text>{user.name}</Text>
       <Text>{user.email}</Text>
-      <Button title="Sign Out" onPress={() => signOut()} />
+      <Button title="Sign Out" onPress={signOut} />
+      <Text>{JSON.stringify(data)}</Text>
+      <Button title="Get Protected Data" onPress={getProtectedData} />
     </View>
   );
 }
